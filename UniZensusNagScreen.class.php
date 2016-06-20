2a34445665643d683e5f68b58cde2981f31ae4e8
noackorama <noackorama@googlemail.com>
2016-06-20 19:28:13 +0200

    nag screen nur einmalig anzeigen, zusätzlicher popuptext für Dozenten

--- UniZensusNagScreen.class.php
+++ UniZensusNagScreen.class.php
@@ -4,15 +4,17 @@ class UniZensusNagScreen extends StudIPPlugin implements SystemPlugin
     function __construct()
     {
         parent::__construct();
-        if (!$GLOBALS['perm']->have_perm('dozent') && match_route('dispatch.php/my_courses')
-            && (time() - $_SESSION['nag_screen_shown']) > (24*60*60)
-        ) {
+        if (match_route('dispatch.php/my_courses')) {
+            $nag_screen_shown = UserConfig::get($GLOBALS['user']->id)->UNIZENSUS_NAG_SCREEN_LAST_SHOWN;
             $user_id = $GLOBALS['user']->id;
 
-            $content_box = addcslashes(formatReady(Config::get()->UNIZENSUSPLUGIN_NAG_SCREEN_CONTENT),"'\n\r");
-            $titel_box = addcslashes(Config::get()->UNIZENSUSPLUGIN_DISPLAYNAME,"'\n\r");
-            $img_url = $this->getPluginUrl($this). '/images/danger.png';
-            $script = <<<EOT
+            if (!$GLOBALS['perm']->have_perm('dozent')
+                && $nag_screen_shown < @Semester::findCurrent()->beginn
+            ) {
+                $content_box = addcslashes(formatReady(Config::get()->UNIZENSUSPLUGIN_NAG_SCREEN_CONTENT), "'\n\r");
+                $titel_box = addcslashes(Config::get()->UNIZENSUSPLUGIN_DISPLAYNAME, "'\n\r");
+                $img_url = $this->getPluginUrl($this) . '/images/danger.png';
+                $script = <<<EOT
 jQuery('document').ready(function(){
     if(jQuery('img[title="Den Fragebogen aufrufen und an der Evaluation teilnehmen"]').length + jQuery('image[alt="Den Fragebogen aufrufen und an der Evaluation teilnehmen"]').length > 0 && jQuery('#UniZensusNagScreenDialogbox').length == 0) {
     STUDIP.UniZensusNagScreen = {};
@@ -30,11 +32,40 @@ jQuery('document').ready(function(){
     }
 });
 EOT;
-
-                $_SESSION['nag_screen_shown'] = time();
-                PageLayout::addHeadElement('script', array('type'=>'text/javascript'), $script);
+                UserConfig::get($GLOBALS['user']->id)->store('UNIZENSUS_NAG_SCREEN_LAST_SHOWN', time());
+                PageLayout::addHeadElement('script', array('type' => 'text/javascript'), $script);
             }
+
+            if ($GLOBALS['perm']->get_perm() == 'dozent'
+                && $nag_screen_shown < @Semester::findCurrent()->beginn
+            ) {
+                $content_box = addcslashes(formatReady(Config::get()->UNIZENSUSPLUGIN_NAG_SCREEN_CONTENT_DOZENT), "'\n\r");
+                $titel_box = addcslashes(Config::get()->UNIZENSUSPLUGIN_DISPLAYNAME, "'\n\r");
+                $img_url = $this->getPluginUrl($this) . '/images/danger.png';
+                $script = <<<EOT
+jQuery('document').ready(function(){
+    if(jQuery('img[title*="Die Evaluation läuft"]').length + jQuery('image[alt*="Die Evaluation läuft"]').length > 0 && jQuery('#UniZensusNagScreenDialogbox').length == 0) {
+    STUDIP.UniZensusNagScreen = {};
+    STUDIP.UniZensusNagScreen.dialog = jQuery('<div id="UniZensusNagScreenDialogbox"><img style="padding: 5px" src="$img_url" align="right"><span>' + '$content_box' + '</span></div>').dialog({
+                       show: '',
+                       hide: 'scale',
+                       title: '$titel_box',
+                       draggable: false,
+                       modal: true,
+                       width: Math.min(600, jQuery(window).width() - 64),
+                       height: 'auto',
+                       maxHeight: jQuery(window).height(),
+                       close: function(){jQuery(this).remove();}
+                     });
+    }
+});
+EOT;
+                UserConfig::get($GLOBALS['user']->id)->store('UNIZENSUS_NAG_SCREEN_LAST_SHOWN', time());
+                PageLayout::addHeadElement('script', array('type' => 'text/javascript'), $script);
+            }
+
         }
+    }
 
 }
 
---
+++ migrations/05_nag_screen_config.php
@@ -0,0 +1,43 @@
+<?php
+class NagScreenConfig extends DBMigration
+{
+    function up()
+    {
+        $query = "INSERT IGNORE INTO `config` (
+                    `config_id`, `parent_id`, `field`, `value`, `is_default`,
+                    `type`, `range`, `section`, `mkdate`, `chdate`, `description`
+                  ) VALUES (
+                    MD5(:field), '', :field, :value, 1, 'string', :range, '',
+                    UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), :description
+                  )";
+        $statement = DBManager::get()->prepare($query);
+        $statement->bindValue(':field', 'UNIZENSUS_NAG_SCREEN_LAST_SHOWN');
+        $statement->bindValue(':value', '0');
+        $statement->bindValue(':range', 'user');
+        $statement->bindValue(':description', 'Speichert wann das Zensus Popup zuletzt angezeigt wurde.');
+        $statement->execute();
+        $statement->bindValue(':field', 'UNIZENSUSPLUGIN_NAG_SCREEN_CONTENT_DOZENT');
+        $statement->bindValue(':value', 'Liebe/r Lehrende,
+
+die aktuelle Lehrveranstaltungsevaluation läuft wieder. Eine möglichst hohe Beteiligung und die Freischaltung der Ergebnisse sind sehr wichtig.
+Bitte motivieren Sie Ihre Studierenden, an der Evaluation teilzunehmen.
+
+In Stud.IP können Sie innerhalb der Veranstaltung unter dem grauen Reiter "Lehrevaluation Unizensus" 
+- die TeilnehmerInnenzahl anpassen,
+- die Ergebnisse Ihrer/m Studiendekan/in und der/m Evaluationsbeauftragten freischalten,
+- die Auswertungsdokumente in- und exklusive der Freitextantworten Ihren Studierenden weiterleiten.
+
+Vielen Dank für Ihre Unterstützung!
+Ihr Referat Studium und Lehre ( evaluation@uni-oldenburg.de )');
+        $statement->bindValue(':range', 'global');
+        $statement->bindValue(':description', 'Text der bei Dozenten nach Start der Evaluierung in einem Popup einmalig angezeigt werden soll (Stud.IP Formatierung).');
+        $statement->execute();
+    }
+
+    function down()
+    {
+        DBManager::get()->exec("DELETE FROM config WHERE field = 'UNIZENSUS_NAG_SCREEN_LAST_SHOWN'");
+        DBManager::get()->exec("DELETE FROM config WHERE field = 'UNIZENSUSPLUGIN_NAG_SCREEN_CONTENT_DOZENT'");
+        DBManager::get()->exec("DELETE FROM user_config WHERE field = 'UNIZENSUS_NAG_SCREEN_LAST_SHOWN'");
+    }
+}
